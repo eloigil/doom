a552739943de5793f3fbea9e78e75464e65cf7b7
Eloi Gil Peiró <eloigilpeiro@Elois-MBP.home>
2018-07-12 02:43:38 +0200

    solved player movement

--- js/objects/player.js
+++ js/objects/player.js
@@ -18,8 +18,8 @@ class Player {
     this._speed = 1;
     // @TODO this should be in trigonometric relationship with _speed for now x, y are 1
     this.speed = {
-      x: 1,
-      y: 1,
+      x: this._speed * Math.cos(this._toRadians(this.angle)),
+      y: this._speed * Math.sin(this._toRadians(this.angle)),
       z: 0
     };
 
@@ -33,7 +33,7 @@ class Player {
     };
 
     this.angle = 0;
-    this._angularSpeed = 5;
+    this._angularSpeed = 3;
   }
 
   move (eventKeys) {
@@ -43,13 +43,28 @@ class Player {
   }
 
   _updatePosition (events) {
-    this.position.x += (events.forwards ? this.speed.y : 0);
+    this.position.x += (events.forwards ? this.speed.x : 0);
+    this.position.y += (events.forwards ? this.speed.y : 0);
+
+    this.position.x -= (events.rightwards ? this.speed.y : 0);
     this.position.y += (events.rightwards ? this.speed.x : 0);
+
     this.position.x -= (events.backwards ? this.speed.x : 0);
-    this.position.y -= (events.leftwards ? this.speed.y : 0);
+    this.position.y -= (events.backwards ? this.speed.y : 0);
+
+    this.position.x += (events.leftwards ? this.speed.y : 0);
+    this.position.y -= (events.leftwards ? this.speed.x : 0);
 
     // update angle also
     this.angle += (events.turnRight ? this._angularSpeed : 0);
     this.angle -= (events.turnLeft ? this._angularSpeed : 0);
+
+    this.speed.x = this._speed * Math.cos(this._toRadians(this.angle));
+    this.speed.y = this._speed * Math.sin(this._toRadians(this.angle));
+  }
+
+  _toRadians (angle) {
+    const angleInRadians = angle * 2 * Math.PI / 360;
+    return angleInRadians;
   }
 }
